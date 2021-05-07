65bf29cc0624bc1c7a3d008eadf90adced580258
POSOkawa <81221324+POSOkawa@users.noreply.github.com>
2021-05-07 15:36:13 +0900

    commit

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,59 @@
 class FurimasController < ApplicationController
+  #before_action :furimas_params, except: [:index, :new, :create]
+
+  
   def index
+    @furimas = Furima.all
+  end
+
+  def new
+    @furima = Furima.new
+  end 
+
+  def create
+    @furima = Furima.new(furimas_params)
+    if @furima.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @furima = Furima.find(params[:id])
+  end
+
+  def edit
+    @furima = Furima.find(params[:id])
+  end
+
+  def update
+    @furima = Furima.find(params[:id])
+    if @furima.update(furimas_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @furima = Furima.find(params[:id])
+  @furima.destroy
+
+    if @furima.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def furimas_params
+    params.require(:furima).permit(:name, :setumei, :price, :image, :basyo_id, :category_id, :futan_id, :hassou_id, :jyoutai_id).merge(user_id: current_user.id)
+  end
+
+  def set_furima
+    @furima = Furima.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'furimas#index'
-  resources :furimas, only: [:new] do
+  resources :furimas, only: [:new, :create, :show, :edit, :update, :destroy] do
+  end
 end
