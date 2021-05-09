1f3beba34100abef031680a00244b25f9bc2f051
POSOkawa <81221324+POSOkawa@users.noreply.github.com>
2021-05-09 12:38:07 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def new
+    @user = User.new(params[:id])
+      if @user.save
+        root_path
+      else
+        render "shared/_error_messages"
+      end
+    end
 end
