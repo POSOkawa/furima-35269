c8bc685276b5727245ba9252732d67bd1bb9882f
POSOkawa <81221324+POSOkawa@users.noreply.github.com>
2021-05-11 11:49:57 +0900

    5111150

--- app/models/futan.rb
+++ app/models/futan.rb
@@ -1,8 +1,8 @@
 class Futan < ActiveHash::Base
     self.data = [
+      { id: 0, name: '--' },
       { id: 1, name: '送料込み' },
-      { id: 2, name: '着払い' },
-      { id: 0, name: '--' }
+      { id: 2, name: '着払い' }
     ]
     include ActiveHash::Associations
     has_many :furimas
--- app/models/hassou.rb
+++ app/models/hassou.rb
@@ -1,10 +1,10 @@
 class Hassou < ActiveHash::Base
     self.data = [
+      { id: 0, name: '--' },
     { id: 1, name: '即' },
      { id: 2, name: '二日ほど' },
       { id: 3, name: '三日ほど' },
-      { id: 4, name: '一週間' },
-     { id: 0, name: '--' }
+      { id: 4, name: '一週間' }
   ]
   include ActiveHash::Associations
     has_many :furimas
