32f5031d3c6bf1bee3e7f7c6bcd219594f00caf8
POSOkawa <81221324+POSOkawa@users.noreply.github.com>
2021-05-14 13:11:12 +0900

    1310

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,6 +1,6 @@
 class FurimasController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_furima, only: [:update, :edit, :show, :destroy]
+  before_action :set_furima, only: [:update, :edit, :show, :destroy, :noorder]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   before_action :noorder, only: [:edit, :update, :destroy]
 
@@ -64,7 +64,6 @@ class FurimasController < ApplicationController
     end
     
     def noorder
-      @furima = Furima.find(params[:id])
     redirect_to root_path if @furima.order != nil
     end
 
